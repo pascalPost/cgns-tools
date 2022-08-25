d0357531cd6730e786f544ca034833a071928df3
Pascal Post <pp-post@mailbox.org>
2022-08-25 14:55:31 +0200

    Fixes for export

--- include/aux.hpp
+++ include/aux.hpp
@@ -5,27 +5,19 @@
 
 #include <type_traits>
 
-namespace cgns {
-
-template<typename T>
-struct always_false : std::false_type
-{
-};
-
-/// @brief helper type for the visitor, see
-/// https://en.cppreference.com/w/cpp/utility/variant/visit
-template<class... Ts>
-struct overloaded : Ts...
-{
-  using Ts::operator()...;
-};
-// explicit deduction guide (not needed as of C++20)
-template<class... Ts>
-overloaded(Ts...) -> overloaded<Ts...>;
-
 #ifdef NDEBUG
 #undef NDEBUG
 #endif
 #include <cassert>
+
+namespace cgns {
+
+template <typename T> struct always_false : std::false_type {};
+
+/// @brief helper type for the visitor, see
+/// https://en.cppreference.com/w/cpp/utility/variant/visit
+template <class... Ts> struct overloaded : Ts... { using Ts::operator()...; };
+// explicit deduction guide (not needed as of C++20)
+template <class... Ts> overloaded(Ts...) -> overloaded<Ts...>;
 
 } // namespace cgns
